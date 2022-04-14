35b4fb51e08d5d671fe29765f1d7e1abeab20dff
Angel Rodriguez <angelrodri@hotmail.com>
2022-04-14 15:08:02 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/* 
+    TV Guide
+    
+    Write an async function called findShow(query)
+        performs a fetch call to:
+        https://api.tvmaze.com/singlesearch/shows?q=${query}&embed=seasons
+        and returns the resulting show object
+        
+    Build a layout to display the show
+        - Title
+        - Summary
+        - Seasons listed as individual divs
+*/
