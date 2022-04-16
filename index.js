6cc5d5e7cb8834ba1a5d1aa84ab5d2be2de0111f
Angel Rodriguez <angelrodri@hotmail.com>
2022-04-16 14:14:57 -0700

    refactored code to show similar shows to The Offic

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-/* 
-    TV Guide
-    
-    Write an async function called findShow(query)
-        performs a fetch call to:
-        https://api.tvmaze.com/singlesearch/shows?q=${query}&embed=seasons
-        and returns the resulting show object
-        
-    Build a layout to display the show
-        - Title
-        - Summary
-        - Seasons listed as individual divs
-*/
-
 async function findShow(query) {
-    const response = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${query}&embed=seasons`);
-    const show = await response.json();
+    let response = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
+    let show = await response.json();
     return show;
 }
 
-findShow('the big bang theory').then(show => {
-    console.log(show);
-    document.body.innerHTML = `
+function getShowHTML(show) {
+    return `
         <div class="my-show">
-            <div class="my-show-title">${show.name}</div>
-
-            <div class="my-show-summary">${show.summary}</div>
-            
-            ${show._embedded.seasons.map(season => {
-                return `<div class="my-show-season">Season ${season.number}</div>`
-            }).join('')}
+            <div class="my-show-title">
+            ${show.name}
+        </div>
+        
+        <div class="my-show-summary">
+            ${show.summary}
+        </div>
         </div>
     `;
-    
-});
+}
+
+function displayShows(shows) {    
+    document.body.innerHTML = `
+        <div class="my-shows">
+        ${shows.map(show => getShowHTML(show.show)).join('')}
+        </div>
+    `; 
+}
+
+findShow('office').then(displayShows)
+    .catch(error => console.log(error));
